e787085ef88c156331311f89949ae0d7be230a7f
MarcoKuo0132 <MarcoKuo0132@github.com>
2020-05-27 10:07:14 +0800

    delete Detonator

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject Explosion;
+    public AudioClip sExplosion;
+    public PhysicMaterial material;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(this.gameObject);
+        GameObject NewExplosion = (GameObject)Instantiate(Explosion, transform.position, Quaternion.identity);
+
+        switch (other.tag)
+        {
+            case "Barrel":
+                other.material = material;
+                break;
+            case "Enemy":
+                other.gameObject.GetComponent<Enemy>().attacked();
+                break;
+            case "Player":
+                other.GetComponent<Player>().attacked();
+                break;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
